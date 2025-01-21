3fda21efc4b0bb41a66e779176f4b1534e94ee32
Jimmy <Jimmy0924TG@gmail.com>
2025-01-21 12:45:39 +0800

    Small test16

--- Backend/index.js
+++ Backend/index.js
@@ -19,9 +19,9 @@ app.use(express.json());
 
 const allowCors = fn => async (req, res) => {
   res.setHeader('Access-Control-Allow-Credentials', true)
-  // res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Origin', '*')
   // another common pattern
-  res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  //res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
   res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
   res.setHeader(
     'Access-Control-Allow-Headers',
