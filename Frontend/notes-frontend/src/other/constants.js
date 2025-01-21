5d8114cff590f60613b7cecf8d9577856d25e63a
Jimmy <Jimmy0924TG@gmail.com>
2025-01-21 12:30:18 +0800

    Small test12

--- Frontend/notes-frontend/src/other/constants.js
+++ Frontend/notes-frontend/src/other/constants.js
@@ -1,3 +1,3 @@
 export const BASE_URL = 'http://localhost:8000'
-export const BASE_URL2 = 'https://to-do-notes-backend.vercel.app/'
+export const BASE_URL2 = 'https://to-do-notes-backend.vercel.app'
 //Test
