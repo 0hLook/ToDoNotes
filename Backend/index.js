47488d73e5042f391f9152b842494267d5bc670f
Jimmy <Jimmy0924TG@gmail.com>
2025-01-22 12:05:14 +0800

    Small test21

--- Backend/index.js
+++ Backend/index.js
@@ -19,35 +19,11 @@ const port = process.env.PORT || 8000;
 
 app.use(express.json());
 
-const allowCors = fn => async (req, res) => {
-  res.setHeader('Access-Control-Allow-Credentials', true)
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  // another common pattern
-  //res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  )
-  if (req.method === 'OPTIONS') {
-    res.status(200).end()
-    return
-  }
-  return await fn(req, res)
-}
-
-const handler = (req, res) => {
-  const d = new Date()
-  res.end(d.toString())
-}
-
-module.exports = allowCors(handler)
-
 app.use(cors({
   origin: "*",
-  methods: ['GET', 'POST', 'OPTIONS', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
-  credentials: true,
+  //methods: ['GET', 'POST', 'OPTIONS', 'PUT', 'DELETE'],
+  //allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
+  //credentials: true,
 }));
 
 
@@ -107,9 +83,9 @@ app.post("/create-account", async (req, res) => {
 // Account Login
 app.post("/login", async (req, res) => {
 
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,PUT,POST,OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization'); 
+  // res.setHeader('Access-Control-Allow-Origin', '*');
+  // res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,PUT,POST,OPTIONS');
+  // res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization'); 
 
   const { email, password } = req.body;
 
@@ -152,9 +128,9 @@ app.post("/login", async (req, res) => {
 // Get User
 app.get("/get-user", authenticateToken, async (req, res) => {
 
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,PUT,POST,OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization'); 
+  // res.setHeader('Access-Control-Allow-Origin', '*');
+  // res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,PUT,POST,OPTIONS');
+  // res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization'); 
 
   const { user } = req.user;
 
@@ -255,9 +231,9 @@ app.put("/edit-note/:noteId", authenticateToken, async (req, res) => {
 // Get All Notes
 app.get("/get-all-notes", authenticateToken, async (req, res) => {
   
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,PUT,POST,OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization'); 
+  // res.setHeader('Access-Control-Allow-Origin', '*');
+  // res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,PUT,POST,OPTIONS');
+  // res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization'); 
 
   const { user } = req.user;
 
