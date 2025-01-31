375863785167746cd85d2a845fda039c0864c9d6
Jimmy <Jimmy0924TG@gmail.com>
2025-01-31 14:19:21 +0800

    Small test31

--- Backend/index.js
+++ Backend/index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 const corsOptions = {
   origin: ['https://to-do-notes-frontend-2.vercel.app'],
   methods: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
-  allowedHeaders: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+  allowedHeaders: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization',
   credentials: true // Allow cookies if needed
 };
 
